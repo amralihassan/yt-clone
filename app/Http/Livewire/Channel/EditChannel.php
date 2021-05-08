64c98fb30eed900a5fbeb53214e246673d970a97
amr <amr_dotnet@ymail.com>
2021-05-08 12:06:41 +0200

    upload logo

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -5,12 +5,15 @@ namespace App\Http\Livewire\Channel;
 use App\Models\Channel;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class EditChannel extends Component
 {
     use AuthorizesRequests;
-    
+    use WithFileUploads;
+
     public $channel;
+    public $image;
 
     protected function rules()
     {
@@ -18,6 +21,7 @@ class EditChannel extends Component
             'channel.name' => 'required|max:100|unique:channels,name,' . $this->channel->id,
             'channel.slug' => 'required|max:100',
             'channel.description' => 'nullable|max:255',
+            'image' => 'nullable|image|max:1024',
         ];
     }
 
@@ -33,7 +37,7 @@ class EditChannel extends Component
 
     public function update()
     {
-        $this->authorize('edit',$this->channel);
+        $this->authorize('edit', $this->channel);
 
         $this->validate();
 
@@ -43,9 +47,19 @@ class EditChannel extends Component
             'description' => $this->channel->description,
         ]);
 
+        // check is form submitted
+        if ($this->image) {
+            // upload image
+            $image = $this->image->storeAs('images', $this->channel->uid . '.png');
+
+            // update image in database
+            $this->channel->update(['image'=>$image]);
+
+        }
+
         session()->flash('message', 'Channel updated');
         // return back();
-        return redirect()->route('channel.edit',['channel'=>$this->channel]);
+        return redirect()->route('channel.edit', ['channel' => $this->channel]);
 
     }
 }
